6d1403bc844df4c4187d6df5754c4dbfb852ad0c
Botond Nas <nas.botond@gmail.com>
2021-12-22 10:47:40 +0100

    add comments

--- TriangulationForStandardStereo/TriangulationForStandardStereo/TriangulationForStandardStereo.cpp
+++ TriangulationForStandardStereo/TriangulationForStandardStereo/TriangulationForStandardStereo.cpp
@@ -36,14 +36,8 @@ Mat read_matrix_from_file(const string file_name, const int rows, const int cols
     }
 
     file_stream.close();
-
     return K;
 }
-
-
-const double threshold = 3.;
-const int ransac_max_iteration = 5000;
-const double ransac_confidence = 0.99;
 
 int main()
 {
@@ -62,7 +56,7 @@ int main()
         cvtColor(img1, img1_gray, COLOR_BGR2GRAY);
         cvtColor(img2, img2_gray, COLOR_BGR2GRAY);
 
-        //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+        // Detect the keypoints using SURF Detector, compute the descriptors
         int minHessian = 1; //400;
         Ptr<SURF> detector = SURF::create(minHessian);
         std::vector<KeyPoint> keypoints1, keypoints2;
@@ -72,13 +66,13 @@ int main()
         cout << "Number of keypoints1: " << keypoints1.size() << endl;
         cout << "Number of keypoints2: " << keypoints2.size() << endl;
 
-        //-- Step 2: Matching descriptor vectors with a FLANN based matcher
+        // Matching descriptor vectors with a FLANN based matcher
         // Since SURF is a floating-point descriptor NORM_L2 is used
         Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
         std::vector< std::vector<DMatch> > knn_matches;
         matcher->knnMatch(descriptors1, descriptors2, knn_matches, 2);
 
-        //-- Filter matches using the Lowe's ratio test
+        // Filter matches using the Lowe's ratio test
         const float ratio_thresh = 0.7f; // 0.5f
         std::vector<DMatch> good_matches;
         for (size_t i = 0; i < knn_matches.size(); i++)
@@ -93,19 +87,16 @@ int main()
         vector<pair<Point2f, Point2f> > pointPairs;
         vector<Point2d> points_img1, points_img2;
 
-        // Calibration matrix
+        // Calibration (camera) matrix
         Mat K = read_matrix_from_file("malaga.txt", 3, 3);
         double deltaX = K.at<double>(0,2);
         // cout << deltaX << endl;
         double deltaY = K.at<double>(1,2);
         // cout << deltaY << endl;
 
+        // Move origin for each dataset to (0, 0) using the u and v parameters from the K matrix
         for (size_t i = 0; i < good_matches.size(); i++)
         {
-            // pair<Point2f, Point2f> currPts;
-            // currPts.first = keypoints2[good_matches[i].trainIdx].pt;
-            // currPts.second = keypoints1[good_matches[i].queryIdx].pt;
-            // pointPairs.push_back(currPts);
             Point2d point1, point2;
             point1 = keypoints1[good_matches[i].queryIdx].pt;
             point2 = keypoints2[good_matches[i].trainIdx].pt;
@@ -119,7 +110,7 @@ int main()
         cout << "Number of correspondences: " << points_img1.size() << endl;
 
         /*
-        //-- Draw matches
+        // Draw matches
         Mat img_matches;
         drawMatches(img1, keypoints1, img2, keypoints2, good_matches, img_matches, Scalar::all(-1),
             Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
@@ -132,8 +123,8 @@ int main()
 
         // IMPLEMENT THE SPECIAL TRIANGULATION METHOD FOR STANDARD STEREO (save point cloud in .xyz or .ply format)
         
-        double b = 120.0; // mm (12 cm)
-        double f = 3.8; // mm
+        double b = 120.0; // mm (12 cm), baseline
+        double f = 3.8; // mm, focal length
         double pixel = 166.67; // pixel/mm
         double u1, u2, v1, d;
         
@@ -143,16 +134,19 @@ int main()
         Point3d newSpatialPoint;
 
         for (int i = 0; i < points_img1.size(); i++)
-        {            
+        {
+            // convert from pixel to mm by dividing by pixel/mm value
             u1 = points_img1[i].x / pixel;
             u2 = points_img2[i].x / pixel;
             v1 = points_img1[i].y / pixel;
-            d = u1 - u2;
+            d = u1 - u2; //disparity
 
-            newSpatialPoint.z = (b * f) / d / 100.0;
-            newSpatialPoint.x = (-b * (u1 + u2)) / (2.0 * d) / 100.0;
-            newSpatialPoint.y = (b * v1) / d / 100.0;
+            // spatial pooints converted to m from mm
+            newSpatialPoint.z = (b * f) / d / 1000.0;
+            newSpatialPoint.x = (-b * (u1 + u2)) / (2.0 * d) / 1000.0;
+            newSpatialPoint.y = (b * v1) / d / 1000.0;
 
+            // constrain to points less than 500 meters away from origin
             if (abs(newSpatialPoint.z) <= 500.0 && newSpatialPoint.z >= 0)
             {
                 spatialPoints.push_back(newSpatialPoint);
@@ -171,14 +165,14 @@ int main()
         Point3i originColor(0, 0, 255);
         colors.push_back(originColor);
 
-        // Write results into a PLY file. 
+        // Write results into a PLY file
         // It can be visualized by open-source 3D application Meshlab (www.meshlab.org)
         std::string s = std::to_string(i);
 
-        char outputFile[12];
+        char outputFile[30];
         strcpy_s(outputFile, "result");
         strcat_s(outputFile, s.c_str());
-        strcat_s(outputFile, ".ply");
+        strcat_s(outputFile, "_demo.ply");
 
         WritePLY(outputFile, spatialPoints, colors);      
     }
